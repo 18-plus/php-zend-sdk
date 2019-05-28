2b84338a493e7715e01278f03cad8b7f0285d615
Aleksandr Stolbov <as.stolbov@gmail.com>
2019-05-28 14:17:11 +0400

    new template

--- src/Module.php
+++ src/Module.php
@@ -14,15 +14,30 @@ class Module
         $baseUrl .= $request->getBaseUrl();
         
         $config = $e->getApplication()->getServiceManager()->get('ApplicationConfig');
-        
-        $title = isset($config['agegate_title']) ? $config['agegate_title'] : '';
-        $testIp = isset($config['agegate_test_ip']) ? $config['agegate_test_ip'] : '';
-        $startFrom = isset($config['agegate_start_from']) ? $config['agegate_start_from'] : '';
-        
+
         $gate = new AgeGate($baseUrl);
-        $gate->setTitle($title);
-        $gate->setTestIp($testIp);
-        $gate->setStartFrom($startFrom);
+        $gate->setTitle($config['agegate_title'] ?? '');
+        $gate->setLogo($config['agegate_logo'] ?? '');
+        
+        $gate->setSiteName($config['agegate_site_name'] ?? '');
+        $gate->setCustomText($config['agegate_custom_text'] ?? '');
+        $gate->setCustomLocation($config['agegate_custom_text_location'] ?? '');
+        
+        $gate->setBackgroundColor($config['agegate_background_color'] ?? '');
+        $gate->setTextColor($config['agegate_text_color'] ?? '');
+        
+        $gate->setRemoveReference($config['agegate_remove_reference'] ?? '');
+        $gate->setRemoveVisiting($config['agegate_remove_visiting'] ?? '');
+        
+        $gate->setTestMode($config['agegate_test_mode'] ?? '');
+        $gate->setTestAnyIp($config['agegate_test_anyip'] ?? '');
+        $gate->setTestIp($config['agegate_test_ip'] ?? '');
+        
+        $gate->setStartFrom($config['agegate_start_from'] ?? '');
+        
+        $gate->setDesktopSessionLifetime($config['agegate_desktop_session_lifetime'] ?? '');
+        $gate->setMobileSessionLifetime($config['agegate_mobile_session_lifetime'] ?? '');
+        
         $gate->run();
     }
 }
